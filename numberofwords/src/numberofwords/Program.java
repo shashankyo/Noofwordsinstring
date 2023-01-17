b0ddb59904c621a9184a928d6ec9e4b400106600
shashank <shashank944533@gmail.com>
2023-01-17 15:09:52 +0530

    first commit

---
+++ numberofwords/src/numberofwords/Program.java
@@ -0,0 +1,34 @@
+package numberofwords;
+
+public class Program {
+public static void main(String[] args) {
+	
+	//1.split method
+	String str = "my name is shashank";
+//	String[] l = str.split(" ");
+	int count = 0;
+//	System.out.println(l.length);
+	
+	//2.using string class
+//	for(int i = 0;i< str.length();i++)
+//	{
+//		if (str.charAt(i) == ' ') {
+//			count ++;
+//		}
+//	}
+//	
+//	System.out.println(count+1);
+	
+	//3.using to char array
+//	char[] ch = str.toCharArray();
+//	for(int i=0;i<ch.length;i++)
+//	{
+//		if(ch[i] == ' ')
+//		{
+//			count ++;
+//		}
+//	}
+//	System.out.println("Count="+(count+1));
+
+}
+}
